2c311aa744697cd042c529a6b96f3e31d3bd55ee
Collins <emiomacollins96@gmail.com>
2021-07-16 10:27:01 +0100

    server.js comments

--- server.js
+++ server.js
@@ -1,41 +1,31 @@
-// express is a web framework for node.js to serve up web pages
 const express = require('express');
-
-// cors is a middleware that enables CORS on all routes
-// (CORS) is a mechanism that enables cross-domain requests
 const cors = require('cors');
-
-// body parser is used to parse the request body
 const bodyParser = require('body-parser');
-
-// path is relative to the root of the project
 const path = require('path');
 
-// if not in production
 if (process.env.NODE_ENV !== 'production') {
-	// require dotenv to load environment variables from .env file
+	// we need to use dotenv to access environment variables when in development
+	// in production heroku passed the environment variables
 	require('dotenv').config();
 }
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
-app.use(bodyParser.json());
-// this is used to parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// for parsing urls
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
-// this is used to serve static files from the public directory
+// specify where to find the static files
 app.use(express.static(__dirname + '/client/build'));
 
-// listen for request on hommepage
+// serve index.html on all routes
 app.get('*', (req, res) => {
-	// send the index.html file
 	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-// port
+// use port 5000 in dev mode
 app.set('port', process.env.PORT || 5000);
-// listen for requests and error
 app.listen(app.get('port'), (err) => {
 	if (err) throw err;
 	console.log('Node app is running on port', app.get('port'));
@@ -51,7 +41,7 @@ app.post('/payment', (req, res) => {
 
 	stripe.charges.create(body, (err, charge) => {
 		if (err) {
-			res.sendStatus(500);
+			res.status(500).send(err);
 		} else {
 			res.sendStatus(200);
 		}
